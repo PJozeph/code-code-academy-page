166f6f9f8c1e2d3f8dac0e3efdb3dc275883f3d6
Pallagi Joe <pallagijoe@gmail.com>
2023-11-04 10:15:25 +0100

    add favicon

--- src/app/home/components/news-letter/news-letter.component.ts
+++ src/app/home/components/news-letter/news-letter.component.ts
@@ -7,11 +7,19 @@ import { FormBuilder, Validators } from '@angular/forms';
     styleUrls: ['./news-letter.component.scss'],
 })
 export class NewsLetterComponent {
+    isEmailSent: boolean = false;
     formBuilder: FormBuilder = inject(FormBuilder);
     formGroup = this.formBuilder.group({
         email: ['', [Validators.required, Validators.email]],
     });
 
     public onSubscribeClick(): void {
+        if (this.formGroup.valid) {
+            this.formGroup.reset();
+            this.isEmailSent = true;
+            setTimeout(() => {
+                this.isEmailSent = false;
+            }, 3000);
+        }
     }
 }
