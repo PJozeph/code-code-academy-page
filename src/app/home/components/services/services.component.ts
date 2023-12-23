206154947ef2ca58a4406cc982a039ecdafaf16c
Pallagi Joe <pallagijoe@gmail.com>
2023-12-23 11:27:56 +0100

    fix typo

--- src/app/home/components/services/services.component.ts
+++ src/app/home/components/services/services.component.ts
@@ -30,7 +30,7 @@ export class ServicesComponent implements OnInit {
         },
         {
             title: 'Egyedi Fejlesztés',
-            description: 'Egeyedi igények alapján fejlesztünk',
+            description: 'Egyedi igények alapján fejlesztünk',
             id: 'custom_development',
             image: '../../../assets/background_code.png',
         },
